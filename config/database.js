c5e3973498428f6f18316349c8a5fd063487336f
admin <cmcreborn@gmail.com>
2017-01-23 15:02:15 +0800

    [add]Test NodeJS with mysql passport

---
+++ config/database.js
@@ -0,0 +1,10 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+        'user': 'root',
+        'password': 'ej03xu35k3'
+    },
+	'database': 'testDB',
+    'users_table': 'users'
+};
